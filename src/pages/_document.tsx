78a5efe082367639e4b85167b4e14ab1d9c78be6
Merli Mejia <merli.mejia@mediacurrent.com>
2024-01-11 21:02:36 -0400

    metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,28 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
+        <meta property="og:title" content="Merli Mejia Tavarez" />
+        <meta
+          property="og:description"
+          content="Software Engineering, Web Development, Mobile Development and Full-Stack Development Services"
+        />
+        <meta
+          property="og:image"
+          content="https://portfolio-fe-rust.vercel.app/images/profile-pic.png"
+        />
+        <meta property="og:url" content="merli.mejiatavarez.com" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Merli Mejia Tavarez" />
+        <meta
+          name="twitter:description"
+          content="Description of your website"
+        />
+        <meta
+          name="twitter:image"
+          content="Software Engineering, Web Development, Mobile Development and Full-Stack Development Services"
+        />
       </Head>
       <body>
         <Main />
